73d205819695d882908bf367a44ed5e2e53ebb58
RandomFractals <RandomFractals@users.noreply.github.com>
2022-01-14 09:48:40 -0600

    add data view types enum for pluggable tabular.data.view config options (#75)
    only Tabulator table and Perspective viewer for now.

---
+++ src/views/dataViewTypes.ts
@@ -0,0 +1,7 @@
+/**
+ * Defines tabular data view types for pluggable tabular.data.view config options.
+ */
+export const enum DataViewTypes {
+	tabulatorTable = 'Tabulator',
+	perspectiveViewer = 'Perspective'
+}
