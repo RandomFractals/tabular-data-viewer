1872570fb75c25b6d3a9b37e5dc35e685abb8941
RandomFractals <RandomFractals@users.noreply.github.com>
2022-02-05 11:53:12 -0600

    create and display tabular data list for a data package (#104)

--- src/commands/listDataResources.ts
+++ src/commands/listDataResources.ts
@@ -11,6 +11,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import { ViewCommands } from './viewCommands';
+import { FileTypes } from '../views/fileTypes';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const DataPackage = require('datapackage');
@@ -25,12 +26,32 @@ export async function registerListDataResourcesCommand(context: ExtensionContext
 
 /**
  * Creates and displays Tabular Data Resources quick pick list for a data package.
+ * 
+ * @param dataPackageUri Local (file:///) or remote/public (https://) data package uri.
  */
 async function listDataResources(dataPackageUri: Uri): Promise<void> {
+	// load data package
 	const dataPackage: any = 
 		await DataPackage.Package.load(dataPackageUri.toString(true)); // skip encoding
-	const dataResources: Array<QuickPickItem> = [];
-	// TODO: extract and display tabular data resources
-	// from the provided data package Uri
 	console.log('dataPackage.resources:', dataPackage.resources);
+
+	// get supported data formats from view file types
+	const supportedDataFormats: Array<string> = Object.values(FileTypes);
+
+	// get data package resources
+	const dataResources: Array<QuickPickItem> = [];
+	dataPackage.resources.forEach((resource: any) => {
+		if (resource.tabular) { // supportedDataFormats.includes(resource.descriptor.format)) {
+			const dataRepositoryUrl: string = ''; // `https://github.com/${dataPackage.user}/${dataPackage.repo}`;
+			dataResources.push({
+				label: `$(table) ${resource.name}`,
+				description: dataPackage.title,
+				detail: resource.source
+			});
+		}
+	});
+
+	// create and display data resources quick pick list
+	const selectedDataResource: QuickPickItem | undefined =
+		await window.showQuickPick(dataResources, { canPickMany: false });
 }
